cea96a8810a3d264fbf0ce426b3203d18e0cc157
astro11 <aquila.strother@asu.edu>
2019-02-08 12:14:10 -0500

    displaying contents of Piggy Bank on each println

--- src/com/lambdaschool/Main.java
+++ src/com/lambdaschool/Main.java
@@ -14,8 +14,15 @@ public class Main
         coinsArrayList.add(new Coins("NicPenkle", 0.05,3));
         coinsArrayList.add(new Coins("Penny", 0.01,10));
 
+        System.out.println("\n **** The Contents of the Piggy Bank ****");
+        System.out.println(coinsArrayList.get(1).quantity + " " + coinsArrayList.get(1).name);
+        System.out.println(coinsArrayList.get(2).quantity + " " + coinsArrayList.get(2).name);
+        System.out.println(coinsArrayList.get(0).quantity + " " + coinsArrayList.get(0).name);
+        System.out.println(coinsArrayList.get(3).quantity + " " + coinsArrayList.get(3).name);
+        System.out.println(coinsArrayList.get(4).quantity + " " + coinsArrayList.get(4).name);
 
-        System.out.println(coinsArrayList.get(1).theValue * coinsArrayList.get(1).quantity);
+
+//        System.out.println(coinsArrayList.get(1).name + " has a whopping " + coinsArrayList.get(1).theValue * coinsArrayList.get(1).quantity);
 
 
 //        for (Coins c : coinsArrayList)
